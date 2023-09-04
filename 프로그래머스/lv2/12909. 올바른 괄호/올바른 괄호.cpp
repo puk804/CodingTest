fc57368532119a0e75a00de4bf9c7778f0668f65
puk804 <143302571+puk804@users.noreply.github.com>
2023-09-04 23:41:07 +0900

    [level 2] Title: 올바른 괄호, Time: 0.16 ms, Memory: 3.81 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.cpp
@@ -0,0 +1,22 @@
+#include<string>
+#include <iostream>
+
+using namespace std;
+
+bool solution(string s)
+{
+    bool answer = true;
+    int left= 0;
+
+    //if(s[s.size()-1] == '(') return false;
+    
+    for(int i=0; i<s.size(); i++){    
+        if(s[i] == '(') left++;
+        else left--;
+
+        if(left==-1) return false;
+    }
+
+    if(left == 0) return answer;
+    else return false;
+}
