39d87305c7294e266b21a475bd8c11abae81afae
puk804 <143302571+puk804@users.noreply.github.com>
2023-09-10 12:09:10 +0900

    [unrated] Title: 추억 점수, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+vector<int> solution(vector<string> name, vector<int> yearning, vector<vector<string>> photo) {
+    vector<int> answer;
+    map<string, int> data;
+    int year;
+    
+    for(int i=0; i<name.size(); ++i){
+        data.insert({name[i], yearning[i]});    
+    }
+    
+    for(int i=0; i<photo.size(); ++i){
+        year = 0;
+        for(int j = 0; j<photo[i].size(); ++j){
+             if(data.find(photo[i][j]) != data.end()){ 
+                year += data[photo[i][j]];
+             }
+            
+        }
+        answer.push_back(year);
+    } 
+    return answer;
+}
